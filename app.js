48be50c41d26c29ab425f79f99e4fbb9eec31f47
seanlin1125 <sean821125@gmail.com>
2022-11-04 17:02:49 +0800

    feat: add express-session and dotenv

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const routes = require('./routes')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+require('./config/mongoose')
+
 const app = express()
 const PORT = 3000
-const routes = require('./routes')
-require('./config/mongoose')
 
 app.engine('.hbs', exphbs.engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}))
 
 app.use(routes)
 
