7ec35e2dc9d664be99fc6d9eddaf521fbc416bde
seanlin1125 <sean821125@gmail.com>
2022-11-04 14:25:32 +0800

    feat: set up express-handlebars

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 const routes = require('./routes')
+
+app.engine('.hbs', exphbs.engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 app.use(routes)
 
