66bedde2cd50e499eaae5ddde15ec68324fbf995
seanlin1125 <sean821125@gmail.com>
2022-11-07 14:01:00 +0800

    feat: add record and category model

---
+++ models/record.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    index: true,
+    required: true
+  },
+  userId: {  // 加入關聯設定
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
+})
+
+const Record = mongoose.model('Record', recordSchema)
+
+module.exports = Record
