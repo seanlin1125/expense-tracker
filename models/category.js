c9f8edf6d8b09ea19b28f1be505dc1cf9990a9a8
seanlin1125 <sean821125@gmail.com>
2022-11-08 14:24:10 +0800

    feat: add categorySeed json data

--- models/category.js
+++ models/category.js
@@ -3,6 +3,11 @@ const Schema = mongoose.Schema
 const categorySchema = new Schema({
   name: {
     type: String,
+    required: true,
+    unique: true
+  },
+  icon: {
+    type: String,
     required: true
   }
 })
